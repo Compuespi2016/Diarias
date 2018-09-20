c84ab7604e22e7a30f17d2cef166f35c7620888a
Compuespi2016 <43246043+Compuespi2016@users.noreply.github.com>
2018-09-20 16:52:50 -0300

    cabeçalho.php
    Cabeçalho

--- cabeçalho.php
+++ cabeçalho.php
@@ -3,15 +3,37 @@
 <head>
 	<title>Index</title>
 
-	 <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<?php
+		include_once "conexao.php";
+		session_start();
+	?>
 </head>
 <body>
 	<header id="cabecalho">
 		<h1>Universidade Estadual do Piauí - UESPI</h1>
+
+		<?php
+			if (!isset($_SESSION['nMatricula'])) {
+		?>
+
 		<nav>
 			<a href="Index.php">Inicio</a>
 			<a href="login.php">Login</a>
 		</nav>
+		<?php
+			}
+			else{
+		?>
+		<nav>
+			<a href="Index.php">Inicio</a>
+			<a href="Solicitar_Diaria.php">Solicitar Diária</a>
+			<a href="sair.php">Sair</a>
+		</nav>
+		<?php
+			}
+		?>
 	</header>
 </body>
 </html>
