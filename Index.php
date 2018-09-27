3d4336915fc9f3e5210a8693112665a12f420494
Compuespi2016 <43246043+Compuespi2016@users.noreply.github.com>
2018-09-27 15:47:13 -0300

    novos arquivos1
    cabeçalho reformulado

--- Index.php
+++ Index.php
@@ -4,10 +4,40 @@
 	<title>Index</title>
 
 	<?php
-		include_once "tela_de_login.php";
+		include_once "cabeçalho.php";
 	?>
+
 </head>
 <body>
-	
+	<?php
+		if (!isset($_SESSION['nMatricula'])) {
+	?>
+
+			<center>
+				<br>
+				<br>
+				<br>
+				<h1>solicitação de diarias</h1>
+				<br>
+				<h4>clique em login para acessar</h4>
+			</center>
+
+	<?php
+	}
+		else{
+	?>
+
+			<center>
+				<br>
+				<br>
+				<br>
+				<h1>Bem-vindo</h1>
+				<br>
+				<h4>clique em solicitar diaria para fazer</h4>
+			</center>
+
+	<?php
+		}
+	?>
 </body>
 </html>
