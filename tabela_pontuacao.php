ad0a5bdaf95dcd08d080af509b26330ae0591f7c
Compuespi2016 <43246043+Compuespi2016@users.noreply.github.com>
2018-10-25 17:57:19 -0300

    novos arquivos6
    Tabela de pontuação incompleta

---
+++ tabela_pontuacao.php
@@ -0,0 +1,463 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Solicitar Diária</title>
+
+	<meta charset="utf-8">
+
+	<?php
+		include_once "cabecalho.php";
+	?>
+
+</head>
+<body>
+	<center>
+	<h1 id="titulo_principal">Solicitação</h1>
+	<br>
+	<form method="post" action="salva_pontuacao.php">
+		<h3>Formulario de Producao Cientifica do Pesquisador</h3>
+		<fieldset id="dados_solicitacao" class="border bg-light rounded">
+			<div class="container">
+			  <div class="row">
+			    <div class="col">
+			      ÁREA DE AVALIAÇÃO
+			    </div>
+			  </div>
+
+
+			  <div class="row">
+			    <div class="col-md-10">
+			      Discriminação (Escore por item) X (Quantidade) = Escore Final
+			    </div>
+			    <div class="col">
+			      Escore por item
+			    </div>
+			    <div class="col">
+			      Quantidade
+			    </div>
+			    <div class="col">
+			      Escore Final
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      1
+			    </div>
+			    <div class="col">
+			      Doutorado
+			    </div>
+			    <div class="col">
+			      30
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      2
+			    </div>
+			    <div class="col">
+			      Mestrado
+			    </div>
+			    <div class="col">
+			      15
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      3
+			    </div>
+			    <div class="col">
+			      Bolsista de Produtividade em Pesquisa do CNPq
+			    </div>
+			    <div class="col">
+			      10
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      4
+			    </div>
+			    <div class="col">
+			      Artigos publicados em periódicos científicos indexados 2**
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis A1
+			    </div>
+			    <div class="col">
+			      10
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis A2
+			    </div>
+			    <div class="col">
+			      8
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis B1, ou trabalho completo publicado em Conferência
+A1, A2 e B1 (especifico para área de Ciência da Computação)
+			    </div>
+			    <div class="col">
+			      6
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis B2, ou trabalho completo publicado em Conferência
+A1, A2 e B1 (especifico para área de Ciência da Computação)
+			    </div>
+			    <div class="col">
+			      4
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis B3, ou trabalho completo publicado em Conferência B2, B3 e
+B4 (especifico para área de Ciência da Computação)
+			    </div>
+			    <div class="col">
+			      3
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis B4; B5
+			    </div>
+			    <div class="col">
+			      2
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      Qualis C
+			    </div>
+			    <div class="col">
+			      1
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+			
+			<div class="row">
+				<div class="col">
+			      5
+			    </div>
+			    <div class="col">
+			      Artigos publicados em periódicos científicos NÃO
+classificados pelo sistema QUALIS 3
+			    </div>
+			    <div class="col">
+			      0.5
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="row">
+				<div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			      
+			    </div>
+			    <div class="col">
+			       <input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			    <div class="col">
+			      	<input type="text" name="DOUTORADO" class="col-sm-7 form-control" id="inputDOUTORADO" required="">
+			    </div>
+			  </div>
+			</div>
+
+
+			<div class="form-group col-md-10">
+				<label class="col-sm-5 col-form-label">Nome:</label>
+	  			<?php echo $_SESSION['nNome']; ?>
+	  		</div>
+  		
+  			<div class="form-group col-md-10">
+  				<label class="col-sm-5 col-form-label">Matrícula: </label>	
+  				<?php echo $_SESSION['nMatricula']; ?>
+			</div>	
+		<br>
+		
+
+
+
+
+
+		<fieldset id="dados_pontuacao" class="border bg-light rounded">
+			<div class="form-group row col-md-12">
+			    <label for="inputNumConta" class="col-sm-5 col-form-label">Número da Conta: </label>
+			    <input type="text" name="nNumConta" class="col-sm-7 form-control" id="inputNumConta" placeholder="Ex: 0000012" required="">
+			</div>
+		</fieldset>
+		
+		</fieldset>
+
+		<br>
+		
+		<input class="btn btn-primary" type="submit" value="Enviar Solicitação">
+
+		<br>
+		<br>
+	</form>
+	</center>
+</body>
+</html>
